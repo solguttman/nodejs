89b10a5c8d1382da5e4d24dcd79a3ea433443237
sol <solguttman@gmail.com>
2015-10-14 10:04:10 -0400

    Basic express routing and jade templating

---
+++ app.js
@@ -0,0 +1,18 @@
+//Module dependencies
+var express = require('express');
+
+// Express settings
+var app = express();
+
+//Template settings
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// Routes
+app.get('/', function(req, res){
+	res.render('pages/landing');
+});
+
+// Start the server
+app.listen(3000);
+console.log('Express running on port 3000');
